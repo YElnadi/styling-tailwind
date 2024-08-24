3c6e0edd83b53b7ed046600d62de8dbd5b58c293
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-08-24 00:03:04 -0700

    first box

--- src/QuoteBox1.js
+++ src/QuoteBox1.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const QuoteBox1 = (props) => {
-    const {quote} = props
-    console.log(quote)
+  const { quote } = props;
+  console.log(quote);
   return (
-    <div>
-      <div className="text-red-500">{quote.text}</div>
-      <div>{quote.author}</div>
-      <div>{quote.bio}</div>
-
+    <div className="flex flex-col m-8 ">
+      <div className="bg-cyan-600 p-10 w-[410px] text-cyan-200 rounded-t-xl">
+        <div>{quote.text}</div>
+      </div>
+      <div className="flex flex-col items-center bg-stone-200 rounded-b-xl p-10">
+        <div className="text-xl text-cyan-700">{quote.author}</div>
+        <div className="text-stone-500">{quote.bio}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default QuoteBox1;
