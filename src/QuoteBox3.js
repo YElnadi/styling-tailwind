22d8cc289c3afa562919b684aee10d57c505ae5b
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-08-24 01:02:35 -0700

    done

--- src/QuoteBox3.js
+++ src/QuoteBox3.js
@@ -4,11 +4,11 @@ const QuoteBox3 = (props) => {
   const { quote } = props;
   console.log("quote3 :", quote);
   return (
-    <div className="flex flex-col m-8">
-      <div className="bg-rose-400 text-stone-100 text-xl w-[500px]  p-10  rounded-t-lg rounded-r-lg">
+    <div className="flex flex-col items-start m-8">
+      <div className="bg-rose-400 text-white text-xl  p-8  rounded-t-lg rounded-br-lg">
         {quote.text}
       </div>
-      <div className="border-2 w-64 p-4 rounded-b-lg bg-rose-100 border-rose-300">
+      <div className=" flex flex-col border-2 w-64 p-4 border-2 rounded-b-lg bg-rose-100 border-rose-300 ">
         <div className="text-xl text-rose-700">{quote.author}</div>
         <div className=" text-rose-400">{quote.bio}</div>
       </div>
