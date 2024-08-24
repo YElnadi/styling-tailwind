85a3a45003b9f6ece858f897b5707d68811c908f
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-08-24 00:40:34 -0700

    box2 done

--- src/QuoteBox2.js
+++ src/QuoteBox2.js
@@ -4,12 +4,12 @@ const QuoteBox2 = (props) => {
   const { quote } = props;
   console.log("quot2", quote);
   return (
-    <div className="flex border rounded-r-full w-[500px]">
-      <div className="rounded-l-xl border p-8 bg-blue-500 ">
-        <div className="text-xl text-white pb-2">{quote.author}</div>
-        <div className="text-blue-800"> {quote.bio}</div>
+    <div className="flex m-8">
+      <div className="rounded-l-lg  p-4 bg-blue-700 w-64 ">
+        <div className="text-2xl text-gray-200 mb-2 ">{quote.author}</div>
+        <div className="text-blue-400"> {quote.bio}</div>
       </div>
-      <div className="flex items-center  p-8 text-slate-500">{quote.text}</div>
+      <div className="flex items-center justify-center px-8 text-stone-500 text-lg w-80 border rounded-r-full border-stone-300 ">{quote.text}</div>
     </div>
   );
 };
