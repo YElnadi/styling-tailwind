4f2acab1fead13bb77e908a67419815329bcc164
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-08-25 00:28:34 -0700

    remove input focue

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
       <div className=" text-8xl text-orange-500 mb-4">Google</div>
       <div className=" flex border w-[600px] p-4 rounded-full bg-orange-300 ">
         <i class="fa-solid fa-magnifying-glass text-xl text-orange-500 ml-1"></i>
-        <input className="flex-1 mx-1 bg-orange-300 " />
+        <input className="flex-1 mx-1 bg-orange-300 focus:outline-none" />
       </div>
       <div className="mt-4">
         <button className="m-2 border rounded-lg px-4 py-2 bg-purple-200 text-purple-600">
